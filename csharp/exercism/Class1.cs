676f50b3a2b8f34cce370179c71321406564e9ef
Chandrakanth <chan18@users.noreply.github.com>
2024-06-25 04:46:43 +0530

    solve QuestLogic

--- csharp/exercism/Class1.cs
+++ csharp/exercism/Class1.cs
@@ -1,6 +1,74 @@
-﻿namespace exercism;
+﻿using System;
 
-public class Class1
+static class QuestLogic
 {
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+        //a fast attack can be made if the knight is sleepin
+        if(knightIsAwake) {
+            return false;
+        }
 
+        return true;
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+        // no one is awake 
+        if (!knightIsAwake  && 
+            !prisonerIsAwake &&
+            !archerIsAwake ) {
+            return false;
+        }
+
+        // every one is awake
+        if (knightIsAwake  && 
+            prisonerIsAwake &&
+            archerIsAwake ) {
+            return true;
+        }
+
+        // if anyone of them is awake
+        if(knightIsAwake  || 
+           prisonerIsAwake ||
+           archerIsAwake  ) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+        if(archerIsAwake == false && prisonerIsAwake == true) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        if(knightIsAwake &&
+            archerIsAwake &&
+            prisonerIsAwake) {
+             return false;
+        } 
+
+        if(petDogIsPresent) {
+             if (archerIsAwake) {
+                  return false;
+             }           
+        } else if (prisonerIsAwake) {        
+            if (!knightIsAwake && !archerIsAwake) {
+                return true;                    
+            }  else {
+                return false;
+            }      
+        } else {
+             return false;
+        }
+
+        return true;
+    }
 }
